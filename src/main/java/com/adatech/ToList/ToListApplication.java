fabe4b1a5fdb7c620aa0b36e5ddbd6428772a094
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-04-07 21:39:23 -0300

    Criação do projeto no Maven

---
+++ src/main/java/com/adatech/ToList/ToListApplication.java
@@ -0,0 +1,13 @@
+package com.adatech.ToList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adatech/ToList/ToListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adatech.ToList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
