0540f824cb74fa6fbd401a99f95411750c4b834a
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-04-13 16:43:22 -0300

    Termino das classes: FilmeOMDB, FilmeVO e Rating do pacote vo

--- src/main/java/com/adatech/IMDB/vo/FilmeVO.java
+++ src/main/java/com/adatech/IMDB/vo/FilmeVO.java
@@ -1,5 +1,33 @@
 package com.adatech.IMDB.vo;
 
-public class FilmeVO {
+import org.springframework.hateoas.RepresentationModel;
 
+public class FilmeVO extends RepresentationModel<FilmeVO> {
+    private Long id;
+    private String title;
+    private String year;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
 }
