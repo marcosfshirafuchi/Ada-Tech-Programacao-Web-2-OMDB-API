cfb23fa28ffd5288a17c9f2b1db5e098736b6ec2
Jana Cruz <jaanasantoss@gmail.com>
2025-04-14 14:38:18 -0300

    alterações classe filme, classe filme dto e interface filme repository

--- src/main/java/com/adatech/IMDB/dto/FilmeDTO.java
+++ src/main/java/com/adatech/IMDB/dto/FilmeDTO.java
@@ -1,4 +1,31 @@
 package com.adatech.IMDB.dto;
 
 public class FilmeDTO {
+    private String Title;
+    private String Year;
+
+
+    //coonstrutor
+    public FilmeDTO(String title, String year) {
+        Title = title;
+        Year = year;
+    }
+    // get e set
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getYear() {
+        return Year;
+    }
+
+    public void setYear(String year) {
+        Year = year;
+    }
+
 }
+
--- src/main/java/com/adatech/IMDB/model/Filme.java
+++ src/main/java/com/adatech/IMDB/model/Filme.java
@@ -1,4 +1,40 @@
 package com.adatech.IMDB.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Filme {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column
+    String title;
+    @Column
+    String year;
+
+    //GET E SET
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        year = year;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
+
